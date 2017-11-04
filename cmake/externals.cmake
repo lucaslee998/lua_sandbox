ed2d09893c735f23546cb31cbfd6f55021ba24d6
Evgeny L <test0x0000@gmail.com>
2017-11-04 17:04:24 +0000

    Use alive fork of lua repository

--- cmake/externals.cmake
+++ cmake/externals.cmake
@@ -64,8 +64,8 @@ else()
     set(LUA_PROJECT "lua-5_1_5")
     externalproject_add(
         ${LUA_PROJECT}
-        GIT_REPOSITORY https://github.com/trink/lua.git
-        GIT_TAG 57d3e82ef270b20dc976f3a6001439589c807793
+        GIT_REPOSITORY https://github.com/rustyrobot/lua.git
+        GIT_TAG e39ed784636c00023b46139d2f0db178a3d56a16
         CMAKE_ARGS ${SANDBOX_CMAKE_ARGS}
         INSTALL_ARGS ${INST_ARGS}
     )
